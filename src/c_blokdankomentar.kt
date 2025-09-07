1383ee5d1acaf3418a387afa3134f6e05b459d1f
heriagustiawan <heriagustiawan.ti@gmail.com>
2025-09-07 19:01:39 +0700

    Blok dan komentar Pertama

---
+++ src/c_blokdankomentar.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Halo ini percobaan saya pada komentar")
+    // ini komentar 1 baris
+    /*
+
+    ini komentar lebih dari satu baris
+
+     */
+
+    /**
+     * kotlin doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
